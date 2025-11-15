c00ece2a4ac7ab1e55c2e77c195f5dbb74eb3597
Hadi Arbabi <H.Arbabi@tejaratshayan.com>
2025-11-15 11:42:57 +0330

    graph started

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloDriver,
+  ApolloDriverConfig,
+} from '@nestjs/apollo';
+import { join } from 'path';
+import { ProductResolver } from './product.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: [
+        join(
+          process.cwd(),
+          'api-gateway/src/schema/**/*.graphql',
+        ),
+      ],
+      sortSchema: true,
+      playground: true,
+      context: ({ req }) => ({
+        authorization: req.headers['authorization'] as
+          | string
+          | undefined,
+        req,
+      }),
+      formatError: (err) => ({
+        message: err.message,
+        path: err.path,
+        extensions: {
+          code: err.extensions?.code ?? 'internal server error',
+          origin: err.extensions ?? null,
+        },
+      }),
+    }),
+  ],
   controllers: [ApiGatewayController],
-  providers: [ApiGatewayService],
+  providers: [ApiGatewayService, ProductResolver],
 })
 export class ApiGatewayModule {}
